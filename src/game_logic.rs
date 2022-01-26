34a5e63539fc5d2882e5f18411721d6908dbddef
A.P.C.S. - monday <tecmann.falce@outlook.de>
2022-01-26 21:00:28 +0100

    [wrdAnlys] put all the game logic stuf in a modul file and added header comments to the methods

---
+++ src/game_logic.rs
@@ -0,0 +1,91 @@
+use std::io;
+
+const AMOUNT_OF_LETTERS: usize = 5;
+
+pub const AMOUNT_OF_TRYS: usize = 5;
+
+/**
+ * this method takes the users answer and compares it with the Word that
+ * is to be guessed. It returns a string that indicates if a user given
+ * letter is in the searched word and if it is at the right possition.
+ * 
+ *     Letter not in the word:    '-'
+ *     Letter in the word:        'o'
+ *     Letter in the right place: The letter itself
+ * 
+ * @users_answer: a borrowed String that contains what will be compared with the
+ *                searched word.
+ * @correct_answer: a borrowed String containing the searched word.
+ * @return: A tupple with a String that shows the the compare result and a boolean
+ *          that indicates, if the Strings are equal.
+ */
+pub fn string_analysis ( users_answer: &String, correct_answer: &String) -> (String, bool) {
+
+    // Placeholder
+    return (String::from("-----"), correct_answer.eq(users_answer));
+
+}
+
+/**
+ * Reads the User Answer form StdIn, removes the linefeed and cuts it to size.
+ * After that, it returns a uppercase coppy of the Input.
+ *
+ * @return: The formated user input
+ */
+pub fn user_input() -> String {
+    let mut buffer = String::new();
+    match io::stdin().read_line(&mut buffer){
+        Ok(_n) => {
+            buffer.remove(buffer.find("\n").unwrap());
+            buffer.truncate(AMOUNT_OF_LETTERS);
+            return buffer.to_ascii_uppercase(); 
+        }
+        Err(_error) => {
+            println!("Error while reading user Input"); 
+            return String::from("\0");
+        } 
+    }
+}
+
+/**
+ * Prints a sentense, that shows the user how many trys they have left.
+ *
+ * @t: the amount of trys left
+ */
+pub fn print_round_announcment(t : usize){
+    if t == 0 {
+        println!("Sie haben {} Versuche",AMOUNT_OF_TRYS);
+    } else {
+        if (AMOUNT_OF_TRYS - t) > 1 {
+            println!("Sie haben noch {} Versuche",AMOUNT_OF_TRYS - t);
+        } else {
+            println!("Sie haben noch einen Versuche");
+        }
+    }
+}
+
+/** 
+ * this method prints the users word followed by the copmare results
+ * right underneath it.
+ * 
+ * @a: a borrowed vector containing the user input strind,
+ *     the compare result string and a boolean indicating if they are
+ *     equal.
+ * @t: the amount tryes.
+ */
+pub fn print_answer_block( a: &Vec<(String, String, bool)>, t: usize) {
+        if t > 0 {
+        println!("\nDeine Antworten waren:\n"); 
+        for i in 0..t {
+            let tup = a.get(i).unwrap();
+            println!("{}", tup.0);
+            print!("{}", tup.1);
+            if tup.2 {
+                println!(" -> {}", tup.2);
+            } else {
+                println!();
+            }
+        }
+    }
+    println!();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,82 +1,22 @@
-use std::io;
-
-const AMOUNT_OF_TRYS: usize = 5;
-const AMOUNT_OF_LETTERS: usize = 5;
-
-fn round_announcer(t : usize){
-    if t == 0 {
-        println!("Sie haben {} Versuche",AMOUNT_OF_TRYS);
-    } else {
-        if (AMOUNT_OF_TRYS - t) > 1 {
-            println!("Sie haben noch {} Versuche",AMOUNT_OF_TRYS - t);
-        } else {
-            println!("Sie haben noch einen Versuche");
-        }
-    }
-}
-
-
-/**
- * Reads the User Answer form StdIn, removes the linefeed and cuts it to size.
- * After that, it returns a uppercase coppy of the Input.
- *
- * @return: The formated user input
- */
-fn user_input() -> String {
-    let mut buffer = String::new();
-    match io::stdin().read_line(&mut buffer){
-        Ok(_n) => {
-            buffer.remove(buffer.find("\n").unwrap());
-            buffer.truncate(AMOUNT_OF_LETTERS);
-            return buffer.to_ascii_uppercase(); 
-        }
-        Err(_error) => {
-            println!("Error while reading user Input"); 
-            return String::from("\0");
-        } 
-    }
-}
-fn print_answer_block( a: &Vec<(String, String, bool)>, t: usize) {
-       if t > 0 {
-        println!("\nDeine Antworten waren:\n"); 
-        for i in 0..t {
-            let tup = a.get(i).unwrap();
-            println!("{}", tup.0);
-            print!("{}", tup.1);
-            if tup.2 {
-                println!(" -> {}", tup.2);
-            } else {
-                println!();
-            }
-        }
-    }
-    println!();
-}
-
-fn string_analysis ( users_answer: &String, correct_answer: &String) -> (String, bool) {
-
-    // Placeholder
-    return (String::from("-----"), correct_answer.eq(users_answer));
-
-}
+mod game_logic;
 
 fn main() {
 
     let mut user_answers: Vec<(String, String, bool)>
-        = vec![(String::new(), String::new(), false); AMOUNT_OF_TRYS];
+        = vec![(String::new(), String::new(), false); game_logic::AMOUNT_OF_TRYS];
   
     // Placeholder for when the Random Word Picker is done
     let word_to_guess: String = String::from("DUMMY");
         
     println!("Erraten sie das Wort!\nEs besteht aus 5 Buchstaben.");
     
-    for rounds in 0 .. AMOUNT_OF_TRYS {
-        round_announcer(rounds);  
-        print_answer_block(&user_answers, rounds);
+    for rounds in 0 .. game_logic::AMOUNT_OF_TRYS {
+        game_logic::print_round_announcment(rounds);  
+        game_logic::print_answer_block(&user_answers, rounds);
 
         // get user input
-        let answer = user_input();
-        let analyzed_answer = string_analysis(&answer,&word_to_guess);
+        let answer = game_logic::user_input();
+        let analyzed_answer = game_logic::string_analysis(&answer,&word_to_guess);
         
         // store user intput string in vector
         let this_rounds_answer = (answer,analyzed_answer.0,analyzed_answer.1);
@@ -84,6 +24,6 @@ fn main() {
         
     }
     
-    print_answer_block(&user_answers, AMOUNT_OF_TRYS);
+    game_logic::print_answer_block(&user_answers, game_logic::AMOUNT_OF_TRYS);
 
 }
