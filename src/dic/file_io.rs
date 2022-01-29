afd0dee919eb01c2489d05be6a7b021daeee3414
A.P.C.S. - monday <tecmann.falce@outlook.de>
2022-01-29 12:21:22 +0100

    [src/dic]:           - directory restructure to satisfy cargo.           - Redone file_io.rs for more general use.

---
+++ src/dic/file_io.rs
@@ -0,0 +1,37 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+
+/**
+ * Opens the dictionary file at a the path.
+ *
+ * @p: path to the dictionary file as a string slice 
+ * 
+ * @return: File type of the dictionary 
+ */
+pub fn open_file(p: &str) -> File {
+    let path = Path::new(p);
+    let display = path.display();
+
+    match File::open(&path) {
+        Err(reason) => panic!("couldn't open {}: {}", display, reason),
+        Ok(file) => return file,
+    };
+}
+
+/**
+ * opens and reads the dictionary file 
+ */
+pub fn read_file(mut f: File) -> String {
+   
+    // This is where the dictionary is going to be dumped in.
+    let mut buffer = String::new();
+
+    match f.read_to_string(&mut buffer) {
+        Err(reason) => panic!("couldn't read file: {}", reason),
+        Ok(..) => {
+            return buffer;
+        } 
+    }
+}
+
