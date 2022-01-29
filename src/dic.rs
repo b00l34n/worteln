16ce005ef16d0caf2d36462117a15d23408a6723
A.P.C.S. - monday <tecmann.falce@outlook.de>
2022-01-29 12:23:03 +0100

    [src/dic] added comment headers and implemented method for word list generating.

--- src/dic.rs
+++ src/dic.rs
@@ -1,20 +1,39 @@
-mod file_io
+use std::collections::HashSet;
+mod file_io;
+
+const PATH_TO_DIC: &str = "./misc/de_DE.dic";
 
 /**
+ * Goes through the dictionary file, find the words that have in the disierd length
+ * in uppercase and put it into an hash set. It also counts the the amount of found
+ * words. Otherwise the set would need to be traversed later again to determen the
+ * amount.
  *
+ * @number_of_letters: the desierd lenth of the words.
+ *
+ * @retrun: a tupple consitend of a set with the found words and the number of 
+ *          found words (whish should be equal to the length of the set).
  */
-pub fn get_list_of_words (number_of_letters : usize) -> (Vec<String>, usize) {
+pub fn get_list_of_words (number_of_letters : usize) -> (HashSet<String>, usize) {
 
-    let mut list_of_words : Vec<String> = vec!();
+    // the vlaues for the return tupple
     let mut word_count : usize = 0;
-    // go through every word in the dictionary
-    for word in file_io::read_dic_file.split_whitespaces() {
+    let mut set_of_words = HashSet::new();
+
+    // open the dictionary and go through every word:
+    for word in file_io::read_file(file_io::open_file(PATH_TO_DIC))
+            .split_whitespace() {
+        
+        // The need to be uppercased befor because of the 
+        // way to_uppercase() handles special characters
+        let word = word.to_uppercase(); 
+        
         // look for words that have the wanted length and shove it into the vector
         if word.char_indices().count() == number_of_letters {
-            list_of_words.push(word);
+            set_of_words.insert(word);
             word_count += 1;
         } 
     }
-    return (list_of_words, word_count);
+    return (set_of_words, word_count);
 }
 
