d0e6213bdd1f01451453362be83becbeb5e0a55a
A.P.C.S. - monday <tecmann.falce@outlook.de>
2022-02-01 23:40:12 +0100

    [src/dic@enum] an attempt was made... I think making this into an enum is the right idea.

--- src/dic.rs
+++ src/dic.rs
@@ -3,57 +3,67 @@ mod file_io;
 
 const PATH_TO_DIC: &str = "./misc/de_DE.dic";
 
-/**
- * Goes through the dictionary file, find the words that have in the disierd length
- * in uppercase and put it into an hash set. It also counts the the amount of found
- * words. Otherwise the set would need to be traversed later again to determen the
- * amount.
- *
- * @number_of_letters: the desierd lenth of the words.
- *
- * @retrun: a tupple consitend of a set with the found words and the number of 
- *          found words (whish should be equal to the length of the set).
- */
-pub fn get_list_of_words (number_of_letters : usize) -> (HashSet<String>, usize) {
-
-    // the vlaues for the return tupple
-    let mut word_count : usize = 0;
-    let mut set_of_words = HashSet::new();
-
-    // open the dictionary and go through every word:
-    for word in file_io::read_file(file_io::open_file(PATH_TO_DIC))
-            .split_whitespace() {
-        
-        // The need to be uppercased befor because of the 
-        // way to_uppercase() handles special characters
-        let word = word.to_uppercase(); 
-        
-        // look for words that have the wanted length and shove it into the vector
-        if word.char_indices().count() == number_of_letters {
-            set_of_words.insert(word);
-            word_count += 1;
-        } 
-    }
-    (set_of_words, word_count)
+enum Dictionary {
+    ListOfWords,
+    AmountOfWords,
 }
 
-/**
- * returns an arbitrary String from the set of Strings that is would
- * be on the n'th position of an itterartor call.
- *
- * @dic: the String Set
- * @n: the n'th possiotion
- *
- * @return: one of the Words in the set
- */
-pub fn get_nth_word(dic: &HashSet<String>, n: usize) -> String {
+impl Dictionary {
     
-    let mut dummy_rator = dic.iter();
-    let result: String;
+    /**
+     * Goes through the dictionary file, find the words that have in the disierd length
+     * in uppercase and put it into an hash set. It also counts the the amount of found
+     * words. Otherwise the set would need to be traversed later again to determen the
+     * amount.
+     *
+     * @word_length: the desierd lenth of the words.
+     *
+     * @retrun: a tupple consitend of a set with the found words and the number of 
+     *          found words (whish should be equal to the length of the set).
+     */
+    fn new(word_length: usize) -> Dictionary {
     
-    match dummy_rator.nth(n) {
-        Some(res) => result = res.to_string(),
-        None => result = String::from("\0"),
+        use crate::dic::Dictionary::*;
+
+        // the vlaues for the return tupple
+        AmountOfWords : usize = 0;
+        ListOfWords = HashSet::new();
+
+        // open the dictionary and go through every word:
+        for word in file_io::read_file(file_io::open_file(PATH_TO_DIC))
+                .split_whitespace() {
+            
+            // The need to be uppercased befor because of the 
+            // way to_uppercase() handles special characters
+            let word = word.to_uppercase(); 
+            
+            // look for words that have the wanted length and shove it into the vector
+            if word.char_indices().count() == word_length {
+                ListOfWords.insert(word);
+                AmountOfWords += 1;
+            } 
+        }
+        Dictionary::{ListOfWords, AmountOfWords} 
     }
-    result
+
+    /**
+     * returns an arbitrary String from the set of Strings that is would
+     * be on the n'th position of an itterartor call.
+     *
+     * @dic: the String Set
+     * @n: the n'th possiotion
+     *
+     * @return: one of the Words in the set
+     */
+    fn get_nth_word(&self, n: usize) -> String {
+    
+        let mut dummy_rator = (ListOfWords).iter();
+        let result: String;
+    
+        match dummy_rator.nth(n) {
+            Some(res) => result = res.to_string(),
+            None => result = String::from("\0"),
+        }
+        result
+    }
 }
