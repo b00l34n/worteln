113dd67b6e5a16ee09a9440f66b20180ccca428e
b00l34n <tecmann.falce@outlook.de>
2022-02-02 09:02:04 +0100

    [src/gl/wordAnalysis] renamed game_logic to gl]

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use rand::Rng;
 
-mod game_logic;
+mod gl;
 mod dic;
 
 fn main() {
     // Get the modifierd dictionary read
-    let dictionary = dic::get_list_of_words(game_logic::AMOUNT_OF_LETTERS);
+    let dictionary = dic::get_list_of_words(gl::AMOUNT_OF_LETTERS);
     
     // Get a random number for the word to guess 
     let random_number = rand::thread_rng().gen_range(0..(dictionary.1) - 1); 
@@ -15,24 +15,24 @@ fn main() {
  
     // init the accumulator vector for all user answer
     let mut user_answers: Vec<(String, String, bool)>
-        = vec![(String::new(), String::new(), false); game_logic::AMOUNT_OF_TRYS];
+        = vec![(String::new(), String::new(), false); gl::AMOUNT_OF_TRYS];
         
     println!("Erraten sie das Wort!\nEs besteht aus 5 Buchstaben.");
     
-    for rounds in 0 .. game_logic::AMOUNT_OF_TRYS {
-        game_logic::print_round_announcment(rounds);  
-        game_logic::print_answer_block(&user_answers, rounds);
+    for rounds in 0 .. gl::AMOUNT_OF_TRYS {
+        gl::print_round_announcment(rounds);  
+        gl::print_answer_block(&user_answers, rounds);
 
         // get user input
-        let answer = game_logic::user_input();
-        let analyzed_answer = game_logic::string_analysis(&answer,&word_to_guess);
+        let answer = gl::user_input();
+        let analyzed_answer = gl::string_analysis(&answer,&word_to_guess);
         
         // store user intput string in vector
         let this_rounds_answer = (answer,analyzed_answer.0,analyzed_answer.1);
         user_answers.insert(rounds, this_rounds_answer);
         
     } 
-    game_logic::print_answer_block(&user_answers, game_logic::AMOUNT_OF_TRYS);
+    gl::print_answer_block(&user_answers, gl::AMOUNT_OF_TRYS);
     println!("{}",word_to_guess);
 }
 
