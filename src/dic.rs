0b0d1d0bf865d5020373b491aa52868455cc4f76
A.P.C.S. - monday <tecmann.falce@outlook.de>
2022-02-05 13:03:14 +0100

    [src/dic] added a method to check if a word is in the dictionary

--- src/dic.rs
+++ src/dic.rs
@@ -13,6 +13,7 @@ pub struct Dictionary {
  * words. Otherwise the set would need to be traversed later again to determen the
  * amount.
  *
+ * @dic_path: a string containing the path to a dic file 
  * @word_length: the desierd lenth of the words.
  *
  * @retrun: a dictionary struct containing the amount of words and the Set of found words 
@@ -41,15 +42,27 @@ pub fn new(dic_path: &str, word_length: usize) -> Dictionary {
 }
 
 /**
+ * Checks if the word is in the Dictionary eg. if it is a real word.
+ *
+ * @word: the word to look for
+ * @d: the dictionary to search the word in
+ *
+ * @return: ture, if the word is found in the dictionary
+ */
+pub fn is_a_word(word: &String, d: &Dictionary) -> bool{
+   return d.word_list.contains(word); 
+}
+
+/**
  * returns an arbitrary String from the set of Strings that is would
  * be on the n'th position of an itterartor call.
  *
- * @dic: the String Set
+ * @d: the dictionary containing a set of words 
  * @n: the n'th possiotion
  *
  * @return: one of the Words in the set
  */
-pub fn get_nth_word(d: Dictionary, n: usize) -> String {
+pub fn get_nth_word(d: &Dictionary, n: usize) -> String {
     
     let mut dummy_rator = (d.word_list).iter();
     let result: String;
--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ fn main() {
     let random_number = rand::thread_rng().gen_range(0..(dictionary.word_count) - 1); 
    
     // A random word from the dictionary
-    let word_to_guess: String = dic::get_nth_word(dictionary,random_number); 
+    let word_to_guess: String = dic::get_nth_word(&dictionary,random_number); 
  
     // init the accumulator vector for all user answer
     let mut user_answers: Vec<(String, String, bool)>
@@ -25,10 +25,17 @@ fn main() {
         game_logic::print_round_announcment(rounds);  
         game_logic::print_answer_block(&user_answers, rounds);
 
-        // get user input
-        let answer = game_logic::user_input();
+        let mut answer : String; 
+
+        // get user input until it's a word
+        loop {
+            answer = game_logic::user_input();
+            if dic::is_a_word(&answer, &dictionary) {
+                break;
+            } 
+        } 
+   
         let analyzed_answer = game_logic::string_analysis(&answer,&word_to_guess);
-        
         // store user intput string in vector
         let this_rounds_answer = (answer,analyzed_answer.0,analyzed_answer.1);
         user_answers.insert(rounds, this_rounds_answer);
