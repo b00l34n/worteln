48a06f5d7add6ed3f74087b7613bed737ad50bdc
A.P.C.S. - monday <tecmann.falce@outlook.de>
2022-02-05 13:26:12 +0100

    [master] resolved conflict

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,7 @@ const PATH_TO_DIC: &str = "./misc/de_DE.dic";
 
 fn main() {
     // Get the modifierd dictionary read
-<<<<<<< HEAD
-    let dictionary = dic::new(PATH_TO_DIC, game_logic::AMOUNT_OF_LETTERS);
-=======
-    let dictionary = dic::get_list_of_words(gl::AMOUNT_OF_LETTERS);
->>>>>>> src/gl@wordAnalysis
+    let dictionary = dic::new(PATH_TO_DIC, gl::AMOUNT_OF_LETTERS);
     
     // Get a random number for the word to guess 
     let random_number = rand::thread_rng().gen_range(0..(dictionary.word_count) - 1); 
@@ -29,12 +25,11 @@ fn main() {
         gl::print_round_announcment(rounds);  
         gl::print_answer_block(&user_answers, rounds);
 
-<<<<<<< HEAD
         let mut answer : String; 
 
         // get user input until it's a word
         loop {
-            answer = game_logic::user_input();
+            answer = gl::user_input();
             if dic::is_a_word(&answer, &dictionary) {
                 break;
             } else {
@@ -42,13 +37,8 @@ fn main() {
             } 
         } 
    
-        let analyzed_answer = game_logic::string_analysis(&answer,&word_to_guess);
-=======
-        // get user input
-        let answer = gl::user_input();
         let analyzed_answer = gl::string_analysis(&answer,&word_to_guess);
         
->>>>>>> src/gl@wordAnalysis
         // store user intput string in vector
         let this_rounds_answer = (answer,analyzed_answer.0,analyzed_answer.1);
         user_answers.insert(rounds, this_rounds_answer);
